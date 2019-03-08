d8c3dcc7e34ffaf767690d6efae3ac3ac5441add
dilipbobby <dilipbobby@users.noreply.github.com>
2019-03-08 14:36:11 +0530

    Update code.py
    parsing then neede values

--- Expert/code.py
+++ Expert/code.py
@@ -7,8 +7,13 @@ import urllib.request, json
 with urllib.request.urlopen("https://itunes.apple.com/us/rss/topalbums/limit=100/json") as url:
 #using json loads conevrting to a string object to json type
     data = json.loads(url.read().decode())
-#Parsing the feed and entry object gives the list of 100 objects
+#Parsing the feed and entry object gives the list of 100 objects where we have the needed data like ID,price,Date etc
     entity=data['feed']['entry']
-    print(entity)
     for obj in entity:
-	    print(obj)
+        print("ID",obj['id']['attributes']['im:id'])
+        print("category",obj['category']['attributes']['label'])
+        print("Name",obj['im:name']['label'])
+        print("artist",obj['im:artist']['label'])
+        print("link",obj['link']['attributes']['href'])
+        print("price",obj['im:price']['label'])
+        print("date",obj['im:releaseDate']['label'])
